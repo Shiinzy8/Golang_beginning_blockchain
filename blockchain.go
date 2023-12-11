a7e26c1cece83ed0f803290efe9e0d2a3bf2df68
Andrii Arkhyliuk <arkhyliuk@redtree.de>
2023-12-11 15:22:45 +0200

    v1.1.3

--- blockchain.go
+++ blockchain.go
@@ -1,6 +1,7 @@
 package Golang_beginning_blockchain
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -18,20 +19,20 @@ type Blockchain struct {
 	genesisBlock Block
 	chain        []Block
 	difficulty   int
-	genesisUser  User
-	users        []User
+	genesisUser  *User
+	users        map[int]*User
 	countUsers   int
 }
 
 func (b *Blockchain) AddDol(user User, amountDol float64) Block {
-	user.addDol(amountDol)
-	return b.addBlock(b.genesisUser, user, AddDol, amountDol, 0)
+	b.users[user.id].addDol(amountDol)
+	return b.addBlock(*b.genesisUser, user, AddDol, amountDol, 0)
 }
 
 func (b *Blockchain) RemoveDol(user User, amountDol float64) (Block, error) {
 	if ok, err := user.checkAmountDol(amountDol); ok {
-		user.removeDol(amountDol)
-		return b.addBlock(user, b.genesisUser, RemoveDol, amountDol, 0), nil
+		b.users[user.id].removeDol(amountDol)
+		return b.addBlock(user, *b.genesisUser, RemoveDol, amountDol, 0), nil
 	} else {
 		return Block{}, err
 	}
@@ -39,8 +40,8 @@ func (b *Blockchain) RemoveDol(user User, amountDol float64) (Block, error) {
 
 func (b *Blockchain) SentBit(fromUser, toUser User, amountBit float64) (Block, error) {
 	if ok, err := fromUser.checkAmountBit(amountBit); ok {
-		fromUser.removeBit(amountBit)
-		toUser.addBit(amountBit)
+		b.users[fromUser.id].removeBit(amountBit)
+		b.users[toUser.id].addBit(amountBit)
 		return b.addBlock(fromUser, toUser, SentBit, 0, amountBit), nil
 	} else {
 		return Block{}, err
@@ -49,9 +50,9 @@ func (b *Blockchain) SentBit(fromUser, toUser User, amountBit float64) (Block, e
 
 func (b *Blockchain) ConvertDolBit(user User, amountDol float64) (Block, error) {
 	if ok, err := user.checkAmountDol(amountDol); ok {
-		user.removeDol(amountDol)
+		b.users[user.id].removeDol(amountDol)
 		amountBit := amountDol / 36000
-		user.addBit(amountBit)
+		b.users[user.id].addBit(amountBit)
 		return b.addBlock(user, user, ConvertDolBit, amountDol, amountBit), nil
 	} else {
 		return Block{}, err
@@ -60,9 +61,9 @@ func (b *Blockchain) ConvertDolBit(user User, amountDol float64) (Block, error) 
 
 func (b *Blockchain) ConvertBitDol(user User, amountBit float64) (Block, error) {
 	if ok, err := user.checkAmountBit(amountBit); ok {
-		user.removeBit(amountBit)
+		b.users[user.id].removeBit(amountBit)
 		amountDol := amountBit * 36000
-		user.addDol(amountDol)
+		b.users[user.id].addDol(amountDol)
 		return b.addBlock(user, user, ConvertBitDol, amountDol, amountBit), nil
 	} else {
 		return Block{}, err
@@ -95,7 +96,7 @@ func CreateBlockchain(difficulty int) Blockchain {
 		timestamp: time.Now(),
 	}
 
-	genesisUser := User{
+	genesisUser := &User{
 		name: "Blockchain",
 		id:   0,
 	}
@@ -105,7 +106,7 @@ func CreateBlockchain(difficulty int) Blockchain {
 		[]Block{genesisBlock},
 		difficulty,
 		genesisUser,
-		[]User{genesisUser},
+		map[int]*User{0: genesisUser},
 		0,
 	}
 }
@@ -113,7 +114,7 @@ func CreateBlockchain(difficulty int) Blockchain {
 func (b *Blockchain) AddUser(name string) User {
 	b.countUsers++
 	newUser := User{name: name, id: b.countUsers, amountBit: 0, amountDol: 0}
-	b.users = append(b.users, newUser)
+	b.users[b.countUsers] = &newUser
 
 	return newUser
 }
@@ -137,14 +138,22 @@ func (b Blockchain) GetGenesisBlock() Block {
 	return b.genesisBlock
 }
 
-func (b Blockchain) GetUsers() []User {
+func (b Blockchain) GetUsers() map[int]*User {
 	return b.users
 }
 
-func (b Blockchain) GetGenesisUser() User {
+func (b Blockchain) GetGenesisUser() *User {
 	return b.genesisUser
 }
 
 func (b Blockchain) GetDifficulty() int {
 	return b.difficulty
+}
+
+func (b Blockchain) PrintUsers() {
+	for _, user := range b.users {
+		fmt.Println(user.name)
+		fmt.Println(user.amountBit, "Bit")
+		fmt.Println(user.amountDol, "Dol")
+	}
 }
